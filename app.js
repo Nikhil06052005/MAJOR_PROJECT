d52de52c3d3d753c6b82c589a734cf1897ac7c9b
Nikhil Dhuriya <165929078+Nikhil06052005@users.noreply.github.com>
2025-08-25 17:21:46 +0530

    25.6.25 ka project phase 2 part c storing and using session info tk ka code h ye

--- app.js
+++ app.js
@@ -1,122 +1,53 @@
-const express= require("express");
-const app= express();
-const mongoose=require("mongoose");
-const Listing = require("./models/listing.js");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
-const ejsMate= require ("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
+const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 //MONGO_DB SE CONNECTION KA CHIJ H YHA PE
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 main()
-.then(()=>{
+  .then(() => {
     console.log("connected to DB");
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 //HOME ROUTE
-app.get("/",(req,res)=>{ 
-    res.send("Hi, I am root");
-})
-
-const validateListing= (req,res,next)=>{
-     let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400, result.error);
-    }else{
-        next();
-    }
-};
-
-//INDEX ROUTE
-app.get("/listings", wrapAsync(async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index", { allListings });
-}));
-
-//NEW ROUTE
-app.get("/listings/new", (req,res)=>{
-    res.render("listings/new.ejs");
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
-//SHOW ROUTE
-app.get("/listings/:id", wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show", {listing});
-}));
-
-//CREATE ROUTE
-app.post("/listings", validateListing, wrapAsync(async (req,res,next)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save(); 
-    res.redirect("/listings");
-}));
-
-//EDIT ROUTE
-app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-})); 
-
-//UPDATE ROUTE
-app.put("/listings/:id", validateListing, wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect("/listings");
-}));
-
-//DELETE ROUTE
-app.delete("/listings/:id", wrapAsync(async (req,res) =>{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-// //TEST_LISTING WALA ROUTE
-// app.get("/testListing", async (req,res)=>{
-//     let sampleListing = new Listing({
-//         title:"My New Villa",
-//         description: "By New Beach",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         Country: "India",
-//     });
-//     await sampleListing.save();
-//     console.log("Sample was saved")
-//     res.send("Sucessfull Testing");
-// })
-
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
 // app.all("*", (req,res,next)=>{
-//     res.send("sand");
+//     next(new ExpressError(404, "Page Not Found"));
 // });
 
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong!" } = err;
-    res.render("error.ejs", { message } )
-    // res.status(statusCode).send(message);
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.render("error.ejs", { message });
 });
 
 //LISTEN KARVANE KA ROUTE
-app.listen(8080, ()=>{
-    console.log("Server is listening to port 8080");
+app.listen(8080, () => {
+  console.log("Server is listening to port 8080");
 });
--- models/listing.js
+++ models/listing.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js"); 
 
 const listingSchema = new Schema({
     title:{
         type: String,
-        required: true,
+        required: true,  
     },
     description:{
         type: String,
@@ -12,16 +13,29 @@ const listingSchema = new Schema({
     },
      image: {
     filename: { type: String, default: "defaultfilename" },
-    url: {
-      type: String,
-      default: "https://unsplash.com/photos/roadside-view-of-mountains-and-sunset-colors-VvIug1kr8yU",
-      set: (v) => v === "" ? "https://unsplash.com/photos/roadside-view-of-mountains-and-sunset-colors-VvIug1kr8yU" : v
-    }
+    url: { 
+    type: String, 
+    default: "https://media.istockphoto.com/id/636484522/photo/hotel-resort-swimming-pool.jpg?s=1024x1024&w=is&k=20&c=zKSS6l9tyGFnoNwp2nfJlzwvuGcQRCuH6YWbObRBgPE=",
+    set: v => v ? v : "https://unsplash.com/photos/roadside-view-of-mountains-and-sunset-colors-VvIug1kr8yU"
+}
+
   },
     price: Number,
     location: String,
     country: String,
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      }
+    ]
 })
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+  if(listing){
+    await Review.deleteMany({_id: {$in: listing.review}});
+  };
+});
 
 const Listing = mongoose.model("Listing", listingSchema );
 module.exports = Listing;
